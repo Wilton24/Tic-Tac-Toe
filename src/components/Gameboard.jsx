931ff32c2b082a83f2c2c40a190838761ec18835
Wilton24 <cbasher24@gmail.com>
2023-11-10 00:37:21 +0800

    disabled the buttons conditionally

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -19,7 +19,10 @@ export default function Gameboard({ onSelectSquare, turns }) {
         <ol key={rowIndex}>
           {row.map((playerSymbol, colIndex) => (
             <li key={colIndex}>
-              <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
+              <button
+                onClick={() => onSelectSquare(rowIndex, colIndex)}
+                disabled={playerSymbol !== null}
+              >
                 {playerSymbol}
               </button>
             </li>
--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,7 +1,11 @@
-export default function Log({turns}) {
+export default function Log({ turns }) {
   return (
     <ol id="log">
-      {turns.map((turn,lmao) => <li key={lmao}>{turn.player}selected{turn.square.row},{turn.square.col}</li>)}
+      {turns.map((turn) => (
+        <li key={`${turn.square.row} ${turn.square.col}`}>
+          {turn.player}selected{turn.square.row},{turn.square.col}
+        </li>
+      ))}
     </ol>
   );
 }
